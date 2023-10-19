b5444b21c27fa176223e527180a64a618c9e8033
MichalZywerKNEI <121889984+MichalZywerKNEI@users.noreply.github.com>
2023-10-19 12:37:08 +0200

    pickup nie dziala

---
+++ Assets/PickupTrash.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupTrash : MonoBehaviour
+{
+    //public Transform Player; //uzywajcie transform zamiast Player
+    public float pickupRange = 2f;
+    public Transform trashBin;
+    public int pointsPerPickup = 10;
+
+    private void Update()
+    {
+        // Sprawdzanie, czy gracz nacisnął klawisz spacji
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Znajdź najbliższe dostępne śmieci
+            GameObject closestPickup = FindClosestPickup();
+
+            // Jeśli znaleziono dostępne śmieci w zasięgu
+            if (closestPickup != null)
+            {
+                // Podnieś śmieci
+                Pickup(closestPickup);
+
+                // Jeśli gracz jest wystarczająco blisko śmietnika, wrzuć śmieci
+                if (IsCloseToTrashBin())
+                {
+                    Dispose();
+                }
+            }
+        }
+    }
+
+    GameObject FindClosestPickup()
+    {
+        GameObject[] pickups = GameObject.FindGameObjectsWithTag("trash");
+        GameObject closest = null;
+        float closestDistance = Mathf.Infinity;
+
+        foreach (GameObject pickup in pickups)
+        {
+            float distance = Vector3.Distance(transform.position, pickup.transform.position);
+            if (distance < pickupRange && distance < closestDistance)
+            {
+                closest = pickup;
+                closestDistance = distance;
+            }
+        }
+
+        return closest;
+    }
+
+    void Pickup(GameObject pickup)
+    {
+        pickup.SetActive(false); // Schowaj śmieci
+    }
+
+    bool IsCloseToTrashBin()
+    {
+        float distance = Vector3.Distance(transform.position, trashBin.position);
+        return distance < pickupRange;
+    }
+
+    void Dispose()
+    {
+        // Tu możesz dodać kod, który przyznaje punkty
+    }
+}
--- Assets/SpawnManager.cs
+++ Assets/SpawnManager.cs
@@ -6,14 +6,25 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField]
     public GameObject[] smieci;
+    private float SpawnRangeX = 11;
+    private float SpawnRangeY = 4;
+
 
     void Start()
     {
         // Pętla przez elementy tablicy i używanie Instantiate dla każdego obiektu
         foreach (GameObject smiec in smieci)
         {
-            Instantiate(smiec);
+            Vector3 randomPosition = GenerateRandomPosition();
+            Instantiate(smiec, randomPosition, Quaternion.identity);
         }
+    }
+
+    Vector3 GenerateRandomPosition()
+    {
+        float spawnX = Random.Range(-SpawnRangeX, SpawnRangeX); // Zakres dla pozycji X
+        float spawnY = Random.Range(-SpawnRangeY, SpawnRangeY); // Zakres dla pozycji Z
+        return new Vector3(spawnX, spawnY, 0); // Y ustawione na 0, ale można dostosować do własnych potrzeb
     }
 
     void Update()
