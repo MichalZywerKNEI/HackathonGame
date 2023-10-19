6e067d16b9bf41a7948bd5cb44e67f1f4e7ebc70
MichalZywerKNEI <121889984+MichalZywerKNEI@users.noreply.github.com>
2023-10-19 11:39:24 +0200

    plz dzialaj

--- Assets/SpawnManager.cs
+++ Assets/SpawnManager.cs
@@ -1,43 +1,23 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField]
-    public GameObject trashPrefab;
-    //public int numberOfTrashObjects = 10;
-   // public float spawnRadius = 10f;
-    private float spawnRange = 9;
+    public GameObject[] smieci;
+
     void Start()
     {
-        //SpawnTrash();
-    }
-
-    private Vector3 RandomPos()
-    {
-        float spawnPosX = Random.Range(-spawnRange, spawnRange);
-        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
-
-        Vector3 randomPos = new Vector3(spawnPosX, spawnPosZ);
-        return randomPos;
-
-
-
-    }
-
-
-    /*void SpawnTrash()
-    {
-        for (int i = 0; i < enemiesToSpaw; i++)
+        // Pętla przez elementy tablicy i używanie Instantiate dla każdego obiektu
+        foreach (GameObject smiec in smieci)
         {
-            int randomEnemy = Random.Range(0, enemyPrefabs.Length);
-
-            Instantiate(enemyPrefabs[randomEnemy], GenerateSpawnPostion(), enemyPrefabs[randomEnemy].transform.rotation);
-
+            Instantiate(smiec);
         }
-    }*/
+    }
 
+    void Update()
+    {
 
-
+    }
 }
